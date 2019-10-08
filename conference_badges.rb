b671e478128f1489c9b3b98bd4cb408421178b52
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-08 18:58:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ end
 
 def assign_rooms(array)
   new_array=[]
-  array.each_with_index do |name|
-    new_array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  array.each_with_index do |name,index|
+    new_array << "Hello, #{name}! You'll be assigned to room #{index}!"
   end
   new_array
 end
