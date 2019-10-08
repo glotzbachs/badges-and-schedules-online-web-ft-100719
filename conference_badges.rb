90f567ee5f8e9ec8043024bec18c74b819b75138
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-08 18:51:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,12 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
+  
+  array.each do |name|
+    
+end  
+
+def assign_rooms(array)
   new_array=[]
   array.each_with_index do |name|
     new_array << "Hello, #{name}! You'll be assigned to room #{name.index}"
